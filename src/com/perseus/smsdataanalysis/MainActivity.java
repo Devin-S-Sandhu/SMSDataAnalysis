f9b69fd80bfe29a4e1507a5c905d1223680ab5a4
Mirrorkirby <devin.sandhu@gmail.com>
2013-10-22 23:19:22 -0500

    logs users 10 most ferquently sent words

--- src/com/perseus/smsdataanalysis/MainActivity.java
+++ src/com/perseus/smsdataanalysis/MainActivity.java
@@ -1,7 +1,17 @@
 package com.perseus.smsdataanalysis;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -9,14 +19,57 @@ import android.widget.EditText;
 import android.util.Log;
 
 public class MainActivity extends Activity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-    private final static String LOG_TAG = "MyFirstApp_tag";
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	private final static String LOG_TAG = "SMSDataAnalysis_tag";
+	private final static String TEH_TAGZ = "YORP";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Log.v(LOG_TAG, "A verbose message");
+		foo();
+	}
+
+	// Bleeding edge development method
+	// Should look like a word frequency
+	private void foo() {
+		String scope = "sent";
+		Cursor cursor = getContentResolver().query(
+				Uri.parse("content://sms/" + scope), new String[] { "body" },
+				null, null, null);
+		cursor.moveToFirst();
+		HashMap<String,Integer> freq = new HashMap<String,Integer>();
+		do {
+			for (int idx = 0; idx < cursor.getColumnCount(); idx++)
+			{
+				for(String s : cursor.getString(idx).split(" "))
+				{
+					if(freq.containsKey(s))
+						freq.put(s, freq.get(s)+1);
+					else
+						freq.put(s, 1);
+				}
+			}
+		} while (cursor.moveToNext());
+		ArrayList<Entry<String, Integer>> out = new ArrayList<Entry<String, Integer>>();
+		out.addAll(freq.entrySet());
+		Collections.sort(out, new Comparator<Entry<String, Integer>>(){
+
+			@Override
+			public int compare(Entry<String, Integer> lhs,
+					Entry<String, Integer> rhs) {
+				// TODO Auto-generated method stub
+				return rhs.getValue() - lhs.getValue(); 
+			}
+
+			
+			
+		});
+		for(int x = 0; x <= 10; x++)
+		{
+			Log.v(TEH_TAGZ, out.get(x).getValue() + " : " + out.get(x).getKey());
+		}
+
 	}
 
 	@Override
