85fb9b462c17e9db53f0e493e28b50391255923c
Mirrorkirby <devin.sandhu@gmail.com>
2013-10-27 23:11:40 -0500

    selecting contacts limits the analysis to those contacts

--- src/com/perseus/smsdataanalysis/Analyzer.java
+++ src/com/perseus/smsdataanalysis/Analyzer.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.content.Context;
 import android.database.Cursor;
@@ -24,6 +26,7 @@ public class Analyzer {
 
 	}
 
+	// Parsing the query and calling the correct method
 	public ArrayList<String> query(String analysisType, String startDate,
 			String endDate, String contacts, Context context) {
 		ArrayList<String> result = new ArrayList<String>();
@@ -46,28 +49,46 @@ public class Analyzer {
 
 	}
 
+	// parses contact strings of the follosing form
+	// NAME <111-111-1111>, NAME <111-111-1111>, etc.
 	private ArrayList<String> parseContacts(String contacts) {
-		// TODO parse contact data
-		return null;
+		ArrayList<String> contactsList = new ArrayList<String>();
+		Pattern pattern = Pattern.compile("[^<]+<([^>]+)>");
+		Matcher matcher = pattern.matcher(contacts);
+		while (matcher.find()) {
+			contactsList.add(matcher.group(1).replaceAll("\\D+",""));
+		}
+		return contactsList;
 	}
 
-	// Bleeding edge development method
-	// Should look like a word frequency
 	private ArrayList<String> wordFrequency(String scope, String startDate,
 			String endDate, ArrayList<String> contactsList, Context context) {
+		// debugging fun!
+		for (String s : contactsList) {
+			Log.v(LOG_TAG, s);
+		}
+		Log.v(LOG_TAG, "--------");
 		Cursor cursor = context.getContentResolver().query(
-				Uri.parse("content://sms/" + scope), new String[] { "body" },
-				null, null, null);
+				Uri.parse("content://sms/" + scope),
+				new String[] { "body", "address" }, null, null, null);
 		cursor.moveToFirst();
 		HashMap<String, Integer> freq = new HashMap<String, Integer>();
 		do {
-			for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
-				for (String s : cursor.getString(idx).split(" ")) {
-					if (freq.containsKey(s))
-						freq.put(s, freq.get(s) + 1);
-					else
-						freq.put(s, 1);
-				}
+//			Checking numbers
+			if(contactsList.size() != 0)
+			{
+				String number = cursor.getString(1);
+				if(number.length() > 10)
+					number.replace("+", "");
+				if(!contactsList.contains(number))
+					continue;
+			}
+			for (String s : cursor.getString(0).split(" ")) {
+				if (freq.containsKey(s))
+					freq.put(s, freq.get(s) + 1);
+				else
+					freq.put(s, 1);
+				Log.v(LOG_TAG, cursor.getString(0) + " from " + cursor.getString(1));
 			}
 		} while (cursor.moveToNext());
 		ArrayList<Entry<String, Integer>> out = new ArrayList<Entry<String, Integer>>();
