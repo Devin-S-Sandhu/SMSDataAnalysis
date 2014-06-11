f0412c0bebd3f9c698c02861994f311959d90aeb
Po-Chen Yang <pc.yang@utexas.edu>
2014-06-11 17:47:44 -0500

    Swap to custom contact picker in Friend Battle

--- src/com/perseus/smsdataanalysis/BattleMenuActivity.java
+++ src/com/perseus/smsdataanalysis/BattleMenuActivity.java
@@ -1,5 +1,7 @@
 package com.perseus.smsdataanalysis;
 
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -36,6 +38,7 @@ public class BattleMenuActivity extends Activity {
 
 		contactOne = contactTwo = "";
 		contactOneNumber = contactTwoNumber = "";
+		SmsUtil.selectedContact = new HashMap<String, String>();
 	}
 
 	public void doBattle(View view) {
@@ -50,7 +53,7 @@ public class BattleMenuActivity extends Activity {
 			intent.putExtra("contactTwoName", contactTwoName);
 			intent.putExtra("timeSpan",
 					((Spinner) findViewById(R.id.time_span)).getSelectedItem()
-							.toString());
+					.toString());
 
 			startActivity(intent);
 		} else {
@@ -63,13 +66,15 @@ public class BattleMenuActivity extends Activity {
 		if (view.getId() == R.id.contact_one) {
 			pickingContactOne = true;
 			pickingContactTwo = false;
+			SmsUtil.selectedContact.remove(contactOneNumber);
 		} else if (view.getId() == R.id.contact_two) {
 			pickingContactOne = false;
 			pickingContactTwo = true;
+			SmsUtil.selectedContact.remove(contactTwoNumber);
 		}
 
-		Intent intent = new Intent(Intent.ACTION_PICK,
-				ContactsContract.Contacts.CONTENT_URI);
+		Intent intent = new Intent(BattleMenuActivity.this,
+				ContactPickerActivity.class);
 		startActivityForResult(intent, CONTACT_PICKER_RESULT);
 	}
 
@@ -77,87 +82,63 @@ public class BattleMenuActivity extends Activity {
 		if (resultCode == RESULT_OK) {
 			switch (requestCode) {
 			case CONTACT_PICKER_RESULT:
-				Uri contactData = data.getData();
-				@SuppressWarnings("deprecation")
-				Cursor c = managedQuery(contactData, null, null, null, null);
-				if (c.moveToFirst()) {
-					String id = c
-							.getString(c
-									.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+				String nameContact = data.getStringExtra("name");
+				String cNumber = data.getStringExtra("ID");
+				
+				ImageView contact_photo = ((ImageView) findViewById(R.id.contact_one_button));
+				Uri u = null;
 
-					String hasPhone = c
-							.getString(c
-									.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+				if (pickingContactOne) {
+					contactOne = new StringBuilder()
+					.append(nameContact).append(" <")
+					.append(cNumber).append(">").append(", ")
+					.toString();
 
-					if (hasPhone.equalsIgnoreCase("1")) {
-						Cursor phones = getContentResolver()
-								.query(ContactsContract.Data.CONTENT_URI,
-										null,
-										ContactsContract.Data.CONTACT_ID
-												+ " = '" + id +"'", null, null);
-						phones.moveToFirst();
-						String cNumber = phones
-								.getString(phones
-										.getColumnIndex(ContactsContract.Data.CONTACT_ID));
+					contactOneNumber = cNumber;
+					contactOneName = nameContact;
 
-						String nameContact = c
-								.getString(c
-										.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
+					TextView label = (TextView) findViewById(R.id.friend_one_label);
+					label.setText(nameContact);
+					u = ContactPhotoHelper.getPhotoUri(this, contactOneNumber);
+				} else if (pickingContactTwo) {
+					contactTwo = new StringBuilder()
+					.append(nameContact).append(" <")
+					.append(cNumber).append(">").append(", ")
+					.toString();
 
-						ImageView contact_photo = ((ImageView) findViewById(R.id.contact_one_button));
-						Uri u = null;
+					contactTwoNumber = cNumber;
+					contactTwoName = nameContact;
 
-						if (pickingContactOne) {
-							contactOne = new StringBuilder()
-									.append(nameContact).append(" <")
-									.append(cNumber).append(">").append(", ")
-									.toString();
+					TextView label = (TextView) findViewById(R.id.friend_two_label);
+					label.setText(nameContact);
+					contact_photo = ((ImageView) findViewById(R.id.contact_two_button));
 
-							contactOneNumber = cNumber;
-							contactOneName = nameContact;
-
-							TextView label = (TextView) findViewById(R.id.friend_one_label);
-							label.setText(nameContact);
-							u = ContactPhotoHelper.getPhotoUri(this, contactOneNumber);
-						} else if (pickingContactTwo) {
-							contactTwo = new StringBuilder()
-									.append(nameContact).append(" <")
-									.append(cNumber).append(">").append(", ")
-									.toString();
-
-							contactTwoNumber = cNumber;
-							contactTwoName = nameContact;
-
-							TextView label = (TextView) findViewById(R.id.friend_two_label);
-							label.setText(nameContact);
-							contact_photo = ((ImageView) findViewById(R.id.contact_two_button));
-
-							u = ContactPhotoHelper.getPhotoUri(this, contactTwoNumber);
-						}
-						
-						if(u != null){
-							contact_photo.getLayoutParams().height = 300;
-							contact_photo.setImageURI(u);
-						}
-						if(contact_photo.getDrawable() == null){
-							String uri = "@drawable/fighter1";
-							if(pickingContactTwo)
-								uri = "@drawable/fighter2";
-							int imageResource = getResources().getIdentifier(uri, null, getApplicationContext().getPackageName());
-							contact_photo.setImageResource(imageResource);
-						}
-						
-						pickingContactOne = false;
-						pickingContactTwo = false;
-					    
-						
-					}
+					u = ContactPhotoHelper.getPhotoUri(this, contactTwoNumber);
 				}
+
+				if(u != null){
+					contact_photo.getLayoutParams().height = 300;
+					contact_photo.setImageURI(u);
+				}
+				if(contact_photo.getDrawable() == null){
+					String uri = "@drawable/fighter1";
+					if(pickingContactTwo)
+						uri = "@drawable/fighter2";
+					int imageResource = getResources().getIdentifier(uri, null, getApplicationContext().getPackageName());
+					contact_photo.setImageResource(imageResource);
+				}
+
+				pickingContactOne = false;
+				pickingContactTwo = false;
 				break;
 			}
 		} else {
-			// gracefully handle failure
-			Log.w(LOG_TAG, "Warning: activity result not ok");
+			if(pickingContactOne && !contactOneNumber.equals("") && !contactOneName.equals(""))
+				SmsUtil.selectedContact.put(contactOneNumber, contactOneName);
+			else if(pickingContactTwo && !contactTwoNumber.equals("") && !contactTwoName.equals(""))
+				SmsUtil.selectedContact.put(contactTwoNumber, contactTwoName);
+			pickingContactOne = false;
+			pickingContactTwo = false;
 		}
 	}
 
---
+++ src/com/perseus/smsdataanalysis/ContactPickerActivity.java
@@ -0,0 +1,232 @@
+package com.perseus.smsdataanalysis;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ContactPickerActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	private final static String LOG_TAG = "ContactPickerActivity";
+
+
+	private ListView mainListView ;
+	private ContactCheckbox[] contacts ;
+	private ArrayAdapter<ContactCheckbox> listAdapter ;
+	EditText inputSearch;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.contact_picker);
+		Log.i(LOG_TAG, "In ContactPickerActivity");
+
+		// Find the ListView resource. 
+		mainListView = (ListView) findViewById( R.id.list_view );
+		inputSearch = (EditText) findViewById(R.id.inputSearch);
+		findViewById(R.id.back).setVisibility(View.GONE);
+
+		mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick( AdapterView<?> parent, View item, 
+					int position, long id) {
+				ContactCheckbox contact = listAdapter.getItem( position );
+				Log.d(LOG_TAG, "Checkbox toggled: name:" + contact.getName() + " number: " +contact.getID() );
+
+				SmsUtil.selectedContact.put(contact.id, contact.name);
+				
+				Intent data = new Intent();
+				data.putExtra("name", contact.getName());
+				data.putExtra("ID", contact.getID());
+				setResult(RESULT_OK, data);
+				finish();
+			}
+		});
+
+
+		contacts = (ContactCheckbox[]) getLastNonConfigurationInstance() ;
+		if ( contacts == null ) {
+			ArrayList<Contact> contactList = SmsUtil.getContacts(getBaseContext());
+			contacts = new ContactCheckbox[contactList.size()-SmsUtil.selectedContact.size()];
+			int i = 0;
+			for(Contact c: contactList)
+			{
+				if(SmsUtil.selectedContact.containsKey(c.id))
+					continue;
+				Uri u = ContactPhotoHelper.getPhotoUri(this, c.id);
+				contacts[i++] = new ContactCheckbox( c.contactName, c.id, u);
+			}
+		}
+		ArrayList<ContactCheckbox> contactList = new ArrayList<ContactCheckbox>();
+		contactList.addAll( Arrays.asList(contacts) );
+
+		// Set our custom array adapter as the ListView's adapter.
+		listAdapter = new ContactArrayAdapter(this, contactList);
+		mainListView.setAdapter( listAdapter );
+
+		/**
+		 * Enabling Search Filter
+		 * */
+		inputSearch.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+				// When user changed the Text
+				ContactPickerActivity.this.listAdapter.getFilter().filter(cs);   
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				// TODO Auto-generated method stub                          
+			}
+		});
+	}
+	
+	public void clearSearch(View view)
+	{
+		inputSearch.setText("");
+	}
+
+	public void back(View view)
+	{
+		finish();
+	}
+	
+	private static class ContactCheckbox {
+		private String name = "" ;
+		private String id = "";
+		private Uri u = null;
+		
+		public ContactCheckbox( String name, String id, Uri u ) {
+			this.setName(name);
+			this.setID(id);
+			this.setUri(u);
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String toString() {
+			return name ; 
+		}
+		public String getID() {
+			return id;
+		}
+		public void setID(String id) {
+			this.id = id;
+		}
+		public Uri getUri() {
+			return u;
+		}
+		public void setUri(Uri u) {
+			this.u = u;
+		}
+	}
+
+	/** Holds child views for one row. */
+	private static class ContactViewHolder {
+		private TextView textView ;
+		private ImageView imageView ;
+		public ContactViewHolder( TextView textView, ImageView imageView ) {
+			this.textView = textView ;
+			this.imageView = imageView ;
+		}
+		public TextView getTextView() {
+			return textView;
+		}
+		public void setTextView(TextView textView) {
+			this.textView = textView;
+		}
+		public ImageView getImageView() {
+			return imageView;
+		}
+		public void setImageView(ImageView imageView) {
+			this.imageView = imageView;
+		}    
+	}
+
+	private static class ContactArrayAdapter extends ArrayAdapter<ContactCheckbox> {
+
+		private LayoutInflater inflater;
+
+		public ContactArrayAdapter( Context context, List<ContactCheckbox> contactList ) {
+			super( context, R.layout.contact_picker_list_item, R.id.rowTextView, contactList );
+			// Cache the LayoutInflate to avoid asking for a new one each time.
+			inflater = LayoutInflater.from(context) ;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ContactCheckbox contact = (ContactCheckbox) this.getItem( position ); 
+
+			// The child views in each row.
+			TextView textView ; 
+			ImageView contact_photo ;
+
+			// Create a new row view
+			if ( convertView == null ) {
+				convertView = inflater.inflate(R.layout.contact_picker_list_item, null);
+
+				// Find the child views.
+				textView = (TextView) convertView.findViewById( R.id.rowTextView );
+				convertView.findViewById( R.id.CheckBox01 ).setVisibility(View.GONE);
+
+				contact_photo = ((ImageView) convertView.findViewById(R.id.contact_photo));
+
+				// Optimization: Tag the row with it's child views, so we don't have to 
+				// call findViewById() later when we reuse the row.
+				convertView.setTag( new ContactViewHolder(textView,contact_photo) );
+			}
+			// Reuse existing row view
+			else {
+				// Because we use a ViewHolder, we avoid having to call findViewById().
+				ContactViewHolder viewHolder = (ContactViewHolder) convertView.getTag();
+				textView = viewHolder.getTextView() ;
+				contact_photo = viewHolder.getImageView();
+			}
+
+			textView.setTag( contact ); 
+			textView.setText(contact.getName() );
+
+			if(contact.getUri() != null)
+				contact_photo.setImageURI(contact.getUri());
+			return convertView;
+		}
+
+	}
+
+	public Object onRetainNonConfigurationInstance() {
+		return contacts ;
+	}
+	
+
+}
