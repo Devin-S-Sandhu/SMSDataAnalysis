d5df730eeb6127c5b59e10a34d6d97ab80dc987f
Po-Chen Yang <pc.yang@utexas.edu>
2014-06-11 14:51:56 -0500

    remove unused function

--- src/com/perseus/smsdataanalysis/BattleResultActivity.java
+++ src/com/perseus/smsdataanalysis/BattleResultActivity.java
@@ -1,6 +1,5 @@
 package com.perseus.smsdataanalysis;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -8,17 +7,11 @@ import java.util.HashMap;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.ContentResolver;
-import android.content.ContentUris;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -273,50 +266,6 @@ public class BattleResultActivity extends Activity {
 			mProgressDialog.dismiss();
 		}
 
-	}
-
-	public static Bitmap loadContactPhoto(ContentResolver cr, long id,
-			long photo_id) {
-
-		Uri uri = ContentUris.withAppendedId(
-				ContactsContract.Contacts.CONTENT_URI, id);
-		InputStream input = ContactsContract.Contacts
-				.openContactPhotoInputStream(cr, uri);
-		if (input != null) {
-			return BitmapFactory.decodeStream(input);
-		} else {
-			Log.d("PHOTO", "first try failed to load photo");
-
-		}
-
-		byte[] photoBytes = null;
-
-		Uri photoUri = ContentUris.withAppendedId(
-				ContactsContract.Data.CONTENT_URI, photo_id);
-
-		Cursor c = cr.query(photoUri,
-				new String[] { ContactsContract.CommonDataKinds.Photo.PHOTO },
-				null, null, null);
-
-		try {
-			if (c.moveToFirst())
-				photoBytes = c.getBlob(0);
-
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-
-		} finally {
-
-			c.close();
-		}
-
-		if (photoBytes != null)
-			return BitmapFactory.decodeByteArray(photoBytes, 0,
-					photoBytes.length);
-		else
-			Log.d("PHOTO", "second try also failed");
-		return null;
 	}
 
 }
